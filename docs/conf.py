94a03f01795dadac67e0ffff6b79a4a45ada3577
Christian S. Perone <christian.perone@gmail.com>
2012-06-30 17:31:16 -0300

    Fixed rtd build.

--- docs/conf.py
+++ docs/conf.py
@@ -29,7 +29,7 @@ class Mock(object):
         else:
             return Mock()
 
-MOCK_MODULES = ['pandas', 'numpy']
+MOCK_MODULES = ['pandas', 'numpy', 'requests']
 for mod_name in MOCK_MODULES:
     sys.modules[mod_name] = Mock()
 
